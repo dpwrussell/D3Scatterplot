3e18cb0e9096bedb6eb216362e5ac77ee1c04dde
dpwrussell <douglas_russell@hms.harvard.edu>
2016-04-22 13:21:06 -0400

    Removed old code

--- components/Picker.jsx
+++ components/Picker.jsx
@@ -3,52 +3,6 @@ import Label from './Label';
 
 export default class Picker extends React.Component {
 
-  // render() {
-  //
-  //   let { targets, hovering, hoveringStart, hoveringEnd } = this.props;
-  //
-  //   if (!targets) {
-  //     return <h1>"Empty Picker"</h1>;
-  //   }
-  //
-  //   let tds = targets.map(target => {
-  //     if (target === hovering) {
-  //       return (
-  //         <td key={ target } >
-  //           <span className="label label-primary"
-  //                 onMouseEnter={ hoveringStart.bind(this, target) }
-  //                 onMouseLeave={ hoveringEnd }>{ target }</span>
-  //         </td>
-  //       );
-  //     }
-  //     return (
-  //       <td key={ target } >
-  //         <span className="label label-default"
-  //               onMouseOver={ this.handleOver }
-  //               onMouseEnter={ hoveringStart.bind(this, target) }
-  //               onMouseLeave={ hoveringEnd }>{ target }</span>
-  //       </td>
-  //     );
-  //   });
-  //
-  //   const chunk = 3;
-  //   let trs = [];
-  //   for (let i=0, len=tds.length; i<len; i+=chunk) {
-  //       trs.push(<tr key={ i }>{ tds.slice(i,i+chunk) }</tr>);
-  //   }
-  //
-  //   return (
-  //     <div className="panel panel-default">
-  //       <div className="panel-heading">Target</div>
-  //       <table className="table">
-  //         <tbody>
-  //           { trs }
-  //         </tbody>
-  //       </table>
-  //     </div>
-  //   );
-  // }
-
   render() {
 
     const { targets, colors, hovering, pivot, selected, hoveringStart, hoveringEnd, selectTarget } = this.props;
